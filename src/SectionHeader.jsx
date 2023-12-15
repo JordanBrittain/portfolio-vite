b928576742e5808713a5a29af6722a1e88ce1236
Jordan <42592124+JordanBrittain@users.noreply.github.com>
2023-12-14 21:08:22 -0600

    added comma to hero

--- src/SectionHeader.jsx
+++ src/SectionHeader.jsx
@@ -2,8 +2,8 @@ function SectionHeader({title}) {
     return (
         <div className="w-full pt-14 pb-8 px-site">
             <div className="border-t border-zinc-200 py-7 flex flex-row gap-3 items-center">
-                <div className="w-2 h-2 bg-zinc-950 rounded-full"></div>
-                <h5 className="text-zinc-950 text-base">{title}</h5>
+                <div className="w-2 h-2 bg-primary-500 rounded-full"></div>
+                <h5 className="text-zinc-950 text-base font-medium">{title}</h5>
             </div>
 
         </div>
