5f18463a968c6e6d0ce55947f80115212d1589d0
Jordan <42592124+JordanBrittain@users.noreply.github.com>
2023-12-26 16:46:04 -0600

    added react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import CaseStudyTile from "./CaseStudyTile";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "profile",
+    element: <CaseStudyTile />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
