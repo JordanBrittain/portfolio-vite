c673789ae98b423059e1ffed430d1add94ddd506
Jordan <42592124+JordanBrittain@users.noreply.github.com>
2024-01-14 15:27:55 -0600

    changed favicon and title

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,7 +6,6 @@ function HeroSection() {
         <div className="px-site w-full flex items-end justify-between flex-1 pb-16 2xl:pb-28">
             <h1 className="font-display group self-center -translate-y-5 text-zinc-950 font-book text-5xl lg:text-7.5xl 2xl:text-9xl w-full lg:w-10/12">
             I'm Jordan, an end-to-end product designer experienced in leading projects on web and mobile apps.
-            
             </h1>
 
 
